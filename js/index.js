0ba1fad8c7ef2624ffd1e554493b1085331f0ce6
Thomas Capello <thomcapello@gmail.com>
2021-03-09 22:45:45 -0500

    Refactored a few event listeners

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // Your code goes here
 
+window.addEventListener("load", function(){
+    const response = prompt("Are you ready to have fun?", "Yes or No");
+    return response
+});
 
-const body = document.querySelector('body')
-body.addEventListener("click", (event)=>{
+const header = document.querySelector('header')
+header.addEventListener("click", (event)=>{
     event.target.style.backgroundColor = "salmon"
 })
 
@@ -12,54 +16,23 @@ logo.addEventListener("mouseover", ()=>{
     logo.style.transition = "transform .5s";
 })
 
-const navigation = document.querySelector('.nav')
-navigation.addEventListener('mouseover', (event) => {
-    if(event.target.style.color === "pink"){
-        event.target.style.color = "black"
-    }else {
-        event.target.style.color = "pink";
-    }
-})
-
-
-const funBusImg = document.querySelectorAll('img')[0]
-funBusImg.addEventListener("auxclick",()=>{
-    funBusImg.style.transform = "rotateY(360deg)";
-    funBusImg.style.transition = "transform 1s";
-})
 const intro = document.querySelector('.intro p')
-intro.addEventListener('select', (event)=>{
-    event.target.style.color = "gray";
-})
+intro.addEventListener('keydown', (event)=>{
+    if(event.key === 9){
+    event.target.style.fontSize = "2rem";
+}
+});
+
 const adventureImg = document.querySelectorAll('img')[1]
 adventureImg.addEventListener('dblclick',(event)=>{
 event.target.style.transform = "rotateZ(360deg)";
 event.target.style.transition = "transform 1s";
+event.stopPropagation();
+
 })
 
 const waterImg = document.querySelectorAll('img')[2]
 waterImg.addEventListener('mousemove', ()=>{
-    waterImg.style.transform = "skewY(360deg)";
-    waterImg.style.transition = "transform 1s"
-})
-
-const destination = document.querySelector('.content-destination p')
-destination.addEventListener('mousedown', ()=>{
-    destination.style.color = "red";
-
-})
-destination.addEventListener('mouseup', ()=>{
-    destination.style.color = "purple"
-
-})
-
-
-
-const boatImg = document.querySelectorAll('img')[3]
-boatImg.addEventListener("mouseenter", function(){
-    boatImg.style.transform = "scale(1.9)";
-    boatImg.style.transition = "transform 0.2s";
-})
-boatImg.addEventListener("mouseleave",()=>{
-    boatImg.style.transform = "scale(1)"
+    waterImg.style.transform = "skew(360deg)";
+    waterImg.style.transition = "transform 2s"
 })
